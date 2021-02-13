725f21b611af1a0601a806bc3b72d0ec1d28f5a3
Sanjeev Kumar <sanjoo.sharma@gmail.com>
2021-02-13 14:35:59 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60279380372cb7001f0e377a",
+				"60279380372cb7001f0e377c"
+			]
+		},
+		"60279380372cb7001f0e377a": {
+			"id": "60279380372cb7001f0e377a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60279380372cb7001f0e377c": {
+			"id": "60279380372cb7001f0e377c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
